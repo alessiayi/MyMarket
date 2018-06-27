9c93d19f02a4d85ce4111d3cb870375b83f116ac
camilaferno <camila.fernandez@utec.edu.pe>
2018-06-27 03:52:06 -0500

    Ultimos cambios

--- RFIDDdatabase/static/js/boleta_admin.js
+++ RFIDDdatabase/static/js/boleta_admin.js
@@ -1,46 +1,54 @@
-var boletas
-$.getJSON("/boletas", function(data){
-  boletas = data["todo"]
-})
+
+function begin(){
+  var boletas
+  $.getJSON("/boletas", function(data){
+    boletas = data["todo"]
+  })
+  rest()
+}
 
 
-$(function(){
-    var dataGrid = $("#gridContainer").dxDataGrid({
-        dataSource: boletas,
-        allowColumnReordering: true,
-        grouping: {
-            autoExpandAll: true,
-        },
-        searchPanel: {
-            visible: true
-        },
-        paging: {
-            pageSize: 10
-        },
-        groupPanel: {
-            visible: true
-        },
-        columns: [
-            "UID_Code",
-            "Nombre_Producto",
-            //"Precio",
-            {
-              dataField: "Precio",
-              alignment: "right",
+function rest(){
+  $(function(){
+      var dataGrid = $("#gridContainer").dxDataGrid({
+          dataSource: boletas,
+          allowColumnReordering: true,
+          grouping: {
+              autoExpandAll: true,
+          },
+          searchPanel: {
+              visible: true
+          },
+          paging: {
+              pageSize: 10
+          },
+          groupPanel: {
+              visible: true
+          },
+          columns: [
+              "UID_Code",
+              "Nombre_Producto",
+              //"Precio",
+              {
+                dataField: "Precio",
+                alignment: "right",
 
-            },
-            {
-                dataField: "Numero_Boleta",
-                groupIndex: 0
-            }
-        ]
-    }).dxDataGrid("instance");
+              },
+              {
+                  dataField: "Numero_Boleta",
+                  groupIndex: 0
+              }
+          ]
+      }).dxDataGrid("instance");
 
-    $("#autoExpand").dxCheckBox({
-        value: true,
-        text: "Expand All Groups",
-        onValueChanged: function(data) {
-            dataGrid.option("grouping.autoExpandAll", data.value);
-        }
-    });
-});
+      $("#autoExpand").dxCheckBox({
+          value: true,
+          text: "Expand All Groups",
+          onValueChanged: function(data) {
+              dataGrid.option("grouping.autoExpandAll", data.value);
+          }
+      });
+  });
+}
+
+$(document).ready(begin());
--- RFIDDdatabase/static/js/boleta_cajero.js
+++ RFIDDdatabase/static/js/boleta_cajero.js
@@ -1,12 +1,25 @@
-var productoscarrito = $.getJSON("/compras")["json_productos"]
-var currentboleta = parseInt($.getJSON("/numboleta"))
-$("#numboleta").text(currentboleta)
 
-function update(){
-  window.location.reload(false)
+var productoscarrito
+var currentboleta
+
+function begin(){
+  $.getJSON("/compras", function(data){
+      productoscarrito = data["json_productos"]
+  })
+
+  $.getJSON("/numboleta", function(data){
+    currentboleta = parseInt(data)
+    $("#numboleta").text(data)
+  })
+
+  rest()
 }
 
 
+function update(){
+  begin()
+  //window.location.reload(false)
+}
 
 
 
@@ -26,32 +39,35 @@ function endReceipt(){
   }, "json");
   */
 }
+function rest(){
+  $(function(){
+      $("#gridContainer").dxDataGrid({
+          dataSource: productoscarrito,
+          columns: [{
+                  dataField: "UID_Code",
+                  width: 130,
+                  caption: "UID_Code"
+              },
+              "Nombre_Producto",
+              {
+                  dataField: "Precio",
+                  alignment: "right",
+                  format: "currency"
+              }
+          ],
+          summary: {
+              totalItems: [{
+                  column: "UID_Code",
+                  summaryType: "count"
+              }, {
+                  column: "Precio",
+                  summaryType: "sum",
+                  valueFormat: "currency"
+              }]
+          }
+      });
+  });
 
-$(function(){
+}
 
-    $("#gridContainer").dxDataGrid({
-        dataSource: productoscarrito,
-        columns: [{
-                dataField: "UID_Code",
-                width: 130,
-                caption: "UID_Code"
-            },
-            "Nombre_Producto",
-            {
-                dataField: "Precio",
-                alignment: "right",
-                format: "currency"
-            }
-        ],
-        summary: {
-            totalItems: [{
-                column: "UID_Code",
-                summaryType: "count"
-            }, {
-                column: "Precio",
-                summaryType: "sum",
-                valueFormat: "currency"
-            }]
-        }
-    });
-});
+$(document).ready(begin());
